3df8a7a435a83d0ae9f1ad77357502b331225712
OCEANOFANYTHING <79260761+OCEANOFANYTHING@users.noreply.github.com>
2021-03-03 12:05:24 +0530

    Add files via upload

---
+++ batch file1.bat
@@ -0,0 +1,16 @@
+echo off
+color 02
+echo                   #####    ##        # ######  ####  #    # 
+echo                   #    #  #  #       # #      #      #    # 
+echo                   #    # #    #      # #####   ####  ###### 
+echo                   #####  ######      # #           # #    # 
+echo                   #   #  #    # #    # #      #    # #    # 
+echo                   #    # #    #  ####  ######  ####  #    # 
+echo  --------------------------------------------------------------
+echo [ IT WILL SHOW YOU ALL FOLDERS AND FILES IN GIVEN DIRECTORY ]
+echo  --------------------------------------------------------------
+pause
+dir C:\Users\naksh\Desktop\python
+pause
+tree
+pause
---
+++ random password generator.bat
@@ -0,0 +1,5 @@
+@echo off
+echo.
+echo              Your Random Password Is - %Random%
+echo.
+pause
