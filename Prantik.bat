0498452ada5a220f7aeb7d5f9d7b7446c0bdb110
OCEANOFANYTHING <79260761+OCEANOFANYTHING@users.noreply.github.com>
2021-03-03 21:28:32 +0530

    Add files via upload

---
+++ Prantik.bat
@@ -0,0 +1,246 @@
+@echo off
+color 02
+echo.
+echo.
+echo.
+echo                                  ######                                      
+echo                                  #     # #####    ##   #    # ##### # #    # 
+echo                                  #     # #    #  #  #  ##   #   #   # #   #  
+echo                                  ######  #    # #    # # #  #   #   # ####   
+echo                                  #       #####  ###### #  # #   #   # #  #   
+echo                                  #       #   #  #    # #   ##   #   # #   #  
+echo                                  #       #    # #    # #    #   #   # #    # 
+echo.
+echo.
+echo                  ------------------------------------------------------------------------
+echo                 [ THIS IS A SERIAL KEY GENERATOR FOR NERO 7 PREMIUM - MADE BY Nakshatra ]
+echo                  ------------------------------------------------------------------------
+echo.
+echo.
+echo.
+echo                    {To make this type of setup files contact us at +91 89021 09455}
+echo.
+echo.
+echo.
+echo                               !!!!!     To generate Serial Key Hit Enter     !!!!!
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo                           your serial key has been generated succesfully !
+echo.
+echo.
+echo.
+echo   your nero 7 premium serial key is -     1C82-0000-19E5-0000-4001-6184-6083          
+echo.
+echo.
+echo.
+echo                     if it does not work hit enter again to generate another serial key
+echo.
+echo.
+echo.
+pause
+color 03
+cls
+echo.
+echo.
+echo.
+echo                           your serial key has been generated succesfully !
+echo.
+echo.
+echo.
+echo   your nero 7 premium serial key is -     1C80-0010-8010-0000-0006-6523-9259          
+echo.
+echo.
+echo.
+echo                     if it does not work hit enter again to generate another serial key
+echo.
+echo.
+pause
+color 04
+cls
+echo.
+echo.
+echo.
+echo                           your serial key has been generated succesfully !
+echo.
+echo.
+echo.
+echo   your nero 7 premium serial key is -     1C80-0010-8010-0000-0007-5491-1454         
+echo.
+echo.
+echo.
+echo                     if it does not work hit enter again to generate another serial key
+echo.
+echo.
+pause
+color 05
+cls
+echo.
+echo.
+echo.
+echo                           your serial key has been generated succesfully !
+echo.
+echo.
+echo.
+echo   your nero 7 premium serial key is -      1C80-0010-8010-0000-0006-2030-4232         
+echo.
+echo.
+echo.
+echo                     if it does not work hit enter again to generate another serial key
+echo.
+echo.
+pause
+color 06
+cls
+echo.
+echo.
+echo.
+echo                           your serial key has been generated succesfully !
+echo.
+echo.
+echo.
+echo   your nero 7 premium serial key is -      1C80-0010-8010-0000-0002-5802-0913         
+echo.
+echo.
+echo.
+echo                     if it does not work hit enter again to generate another serial key
+echo.
+echo.
+pause
+color 07
+cls
+echo.
+echo.
+echo.
+echo                           your serial key has been generated succesfully !
+echo.
+echo.
+echo.
+echo   your nero 7 premium serial key is -      1C80-0010-8010-0000-0008-0245-1785         
+echo.
+echo.
+echo.
+echo                     if it does not work hit enter again to generate another serial key
+echo.
+echo.
+pause
+color 08
+cls
+echo.
+echo.
+echo.
+echo                           your serial key has been generated succesfully !
+echo.
+echo.
+echo.
+echo   your nero 7 premium serial key is -      1C80-0010-8010-0000-0003-7287-1623        
+echo.
+echo.
+echo.
+echo                     if it does not work hit enter again to generate another serial key
+echo.
+echo.
+pause
+color 09
+cls
+echo.
+echo.
+echo.
+echo                           your serial key has been generated succesfully !
+echo.
+echo.
+echo.
+echo   your nero 7 premium serial key is -       1C80-0010-8010-0000-0008-1224-6871        
+echo.
+echo.
+echo.
+echo                     if it does not work hit enter again to generate another serial key
+echo.
+echo.
+pause
+color 01
+cls
+echo.
+echo.
+echo.
+echo                           your serial key has been generated succesfully !
+echo.
+echo.
+echo.
+echo   your nero 7 premium serial key is -       1C80-0010-8010-0000-0008-1224-6871        
+echo.
+echo.
+echo.
+echo                     if it does not work hit enter again to generate the last serial key
+echo.
+echo.
+pause
+color 02
+cls
+echo.
+echo.
+echo.
+echo                       your last serial key has been generated succesfully !
+echo.
+echo.
+echo.
+echo   your nero 7 premium serial key is -         1C80-0010-8010-0000-0002-0960-7879        
+echo.
+echo.
+echo.
+echo                     if it does not work your pc is a piece of shit !  now buy a new one
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                      thanks for using my key generator
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
+color 07
+cls
+echo.
+echo.
+echo                                            #####      contact us    #####
+echo.
+echo.
+echo.
+echo.
+echo                            IF YOU WANT TO MAKE THIS TYPE OF SETUP FILE THEN CONTACT US
+echo.
+echo.
+echo.
+echo                                    OUR OFFICIAL EMAIL IS - virusserver8697@gmail.com
+echo.
+echo.
+echo.
+echo                             OUR OFFICIAL WEBSITE - https://www.oceanofanything99..blogspot.com
+echo.
+echo.
+echo.
+echo                              OUR PERSONAL OFFICILA CONTACT NUMBER IS - +91 89021 09455
+echo.
+echo.
+echo                                                    PRICE - 1 $ ONLY
+echo.
+echo.
+echo                                      press any key to exit key generator
+echo.
+pause
--- batch file1.bat
+++ batch file1.bat
@@ -1,16 +1,32 @@
-echo off
+@echo off
 color 02
+echo.
+echo.
+echo.
 echo                   #####    ##        # ######  ####  #    # 
 echo                   #    #  #  #       # #      #      #    # 
 echo                   #    # #    #      # #####   ####  ###### 
 echo                   #####  ######      # #           # #    # 
 echo                   #   #  #    # #    # #      #    # #    # 
 echo                   #    # #    #  ####  ######  ####  #    # 
-echo  --------------------------------------------------------------
-echo [ IT WILL SHOW YOU ALL FOLDERS AND FILES IN GIVEN DIRECTORY ]
-echo  --------------------------------------------------------------
+echo.
+echo.
+echo.
+echo        --------------------------------------------------------------
+echo       [ IT WILL SHOW YOU ALL FOLDERS AND FILES IN GIVEN DIRECTORY ]
+echo        --------------------------------------------------------------
+echo.
+echo.
+echo.
 pause
-dir C:\Users\naksh\Desktop\python
+echo.
+echo.
+echo                                        HAGGU           
+echo.
+echo.
+echo.
+pause
+dir C:\Users\naksh\Desktop
 pause
 tree
 pause
